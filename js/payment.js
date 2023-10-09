c4ada954536b78a45d702519d2dbd6c2035556c6
felliiptr <111947400+feliiptr@users.noreply.github.com>
2023-10-09 10:50:53 +0700

    added payment type on payment

--- js/payment.js
+++ js/payment.js
@@ -1,9 +1,25 @@
+const selectElement = document.getElementById("payment-type");
+const phoneNoInput = document.getElementById("uang-elektronik");
+const cardInput = document.getElementById("type-card");
+
+selectElement.addEventListener("change", function () {
+  const selectedOption = selectElement.value;
+  phoneNoInput.style.display = "none";
+  cardInput.style.display = "none";
+
+  if (selectedOption === "uang-elektronik") {
+    phoneNoInput.style.display = "block";
+  } else if (selectedOption === "card") {
+    cardInput.style.display = "block";
+  }
+});
+
 function showLoading(event, button) {
   event.preventDefault();
-  button.innerHTML = "Processing Payment...";
+  button.innerHTML = "Memproses pembayaran...";
 
   setTimeout(function () {
-    button.innerHTML = "Payment completed.";
+    button.innerHTML = "Pembayaran dikonfirmasi";
 
     setTimeout(function () {
       window.location.href = "/Pages/index1.html";

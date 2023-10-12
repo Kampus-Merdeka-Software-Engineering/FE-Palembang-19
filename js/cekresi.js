e2f20ca6d72ef5b9064b5bd2e7cb9f65e476ec51
felliiptr <111947400+feliiptr@users.noreply.github.com>
2023-10-12 15:47:54 +0700

    minor fixes

---
+++ js/cekresi.js
@@ -0,0 +1,62 @@
+const imgdibuat = document.getElementById("img1");
+const imgpengiriman = document.getElementById("img2");
+const imgditerima = document.getElementById("img3");
+
+const gariskiri = document.getElementById("grs1");
+const gariskanan = document.getElementById("grs2");
+
+const container2 = document.getElementById("container-2");
+
+const resi = document.getElementById("resi");
+const penerima = document.getElementById("penerima");
+const notiferror = document.getElementById("notiferror");
+
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchbar");
+
+searchInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const inputValue = searchInput.value;
+
+    if (inputValue === "HRZN001") {
+      imgdibuat.style.backgroundColor = "#84a98c";
+      imgpengiriman.style.backgroundColor = "#84a98c";
+      imgditerima.style.backgroundColor = "#84a98c";
+      gariskiri.style.backgroundColor = "#84a98c";
+      gariskanan.style.backgroundColor = "#84a98c";
+      container2.style.display = "flex";
+      resi.textContent = inputValue;
+      penerima.textContent = "Fellia Puteri";
+      notiferror.style.display = "none";
+    } else if (inputValue === "HRZN002") {
+      imgdibuat.style.backgroundColor = "#84a98c";
+      gariskiri.style.backgroundColor = "#84a98c";
+      imgpengiriman.style.backgroundColor = "#84a98c";
+      gariskanan.style.backgroundColor = "#fff";
+      imgditerima.style.backgroundColor = "#fff";
+      container2.style.display = "flex";
+      resi.textContent = inputValue;
+      penerima.textContent = "Khowash";
+      notiferror.style.display = "none";
+    } else if (inputValue === "HRZN003") {
+      imgdibuat.style.backgroundColor = "#84a98c";
+      gariskiri.style.backgroundColor = "#fff";
+      imgpengiriman.style.backgroundColor = "#fff";
+      gariskanan.style.backgroundColor = "#fff";
+      imgditerima.style.backgroundColor = "#fff";
+      container2.style.display = "flex";
+      resi.textContent = inputValue;
+      penerima.textContent = "Mala";
+      notiferror.style.display = "none";
+    } else {
+      imgdibuat.style.backgroundColor = "#fff";
+      imgpengiriman.style.backgroundColor = "#fff";
+      imgditerima.style.backgroundColor = "#fff";
+      gariskiri.style.backgroundColor = "#fff";
+      gariskanan.style.backgroundColor = "#fff";
+      container2.style.display = "none";
+      notiferror.style.display = "block";
+    }
+  }
+});
--- js/validation.js
+++ js/validation.js
@@ -1,22 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("myForm");
+const nama = document.getElementById("nama-lengkap");
+const alamat = document.getElementById("alamat");
+const telepon = document.getElementById("no-telepon");
+const nama2 = document.getElementById("nama-lengkap2");
+const alamat2 = document.getElementById("alamat2");
+const telepon2 = document.getElementById("no-telepon2");
+const berat = document.getElementById("berat-barang");
+const barang = document.getElementById("tipe-barang");
+const layanan = document.getElementById("tipe-layanan");
+const metode = document.getElementById("metode");
 
-  form.addEventListener("submit", function (event) {
+const linkElement = document.querySelector('a[href="#"]');
+const btnsend = document.getElementById("btnsend");
+
+const notiferror = document.getElementById("notiferror");
+
+btnsend.addEventListener("click", function (event) {
+  if (
+    nama.value === "" ||
+    alamat.value === "" ||
+    telepon.value === "" ||
+    nama2.value === "" ||
+    alamat2.value === "" ||
+    telepon2.value === "" ||
+    berat.value === "" ||
+    barang.value === "" ||
+    layanan.value === "" ||
+    metode.value === ""
+  ) {
+    notiferror.style.display = "block";
+    notiferror.style.opacity = 1;
     event.preventDefault();
 
-    const inputs = form.querySelectorAll("input");
-
-    let allFilled = true;
-    inputs.forEach(function (input) {
-      if (!input.value) {
-        allFilled = false;
-      }
-    });
-
-    if (allFilled) {
-      form.submit();
-    } else {
-      alert("Please fill in all required fields.");
-    }
-  });
+    setTimeout(function () {
+      notiferror.style.opacity = 0;
+      setTimeout(function () {
+        notiferror.style.display = "none";
+      }, 500);
+    }, 5000);
+  } else {
+    notiferror.style.display = "none";
+    linkElement.href = "#popup";
+  }
 });

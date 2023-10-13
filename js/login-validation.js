6281a8642a45164b6412db919a64f4a39261630f
felliiptr <111947400+feliiptr@users.noreply.github.com>
2023-10-13 16:38:46 +0700

    added login for admin

--- js/login-validation.js
+++ js/login-validation.js
@@ -5,10 +5,12 @@ const linkElement = document.getElementById("toindex");
 const btnlogin = document.getElementById("btnlogin");
 
 const notiferror = document.getElementById("notiferror");
+const notifwrong = document.getElementById("notifwrong");
 
 btnlogin.addEventListener("click", function (event) {
   if (username.value === "" || password.value === "") {
     notiferror.style.display = "block";
+    notifwrong.style.display = "none";
     notiferror.style.opacity = 1;
     event.preventDefault();
 
@@ -18,8 +20,13 @@ btnlogin.addEventListener("click", function (event) {
         notiferror.style.display = "none";
       }, 500);
     }, 5000);
+  } else if (username.value === "admin" || password.value === "admin") {
+    notiferror.style.display = "none";
+    notifwrong.style.display = "none";
+    linkElement.href = "/Pages/adm-index.html";
   } else {
     notiferror.style.display = "none";
+    notifwrong.style.display = "none";
     linkElement.href = "/Pages/index1.html";
   }
 });
